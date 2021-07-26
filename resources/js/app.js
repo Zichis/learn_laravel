cfbce5b346840a252119e6e9b24c88e4c9bf0c5a
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-07-26 17:15:15 +0100

    Updated app.js with vue.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+window.Vue = require('vue');
+
+Vue.component('example-component', ExampleComponent);
+
+const app = new Vue({
+    el: '#app'
+});
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h3>Hell Guys!</h3>
+    </div>
+</template>

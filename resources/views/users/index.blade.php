385aac21c41da8b9b361bfd1c49dbf9456d4fa6f
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-07-26 17:16:39 +0100

    Added layout to users index page.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,5 +1,16 @@
-@forelse ($users as $user)
-    <p>{{ $user->name }}</p>
-@empty
-    <p>No user found</p>
-@endforelse
+@extends('layouts.main')
+
+@section('title', 'Users')
+
+@section('section')
+    <example-component></example-component>
+    @forelse ($users as $user)
+        <p>{{ $user->name }}</p>
+        @empty
+        <p>No user found</p>
+    @endforelse
+@endsection
+
+@section('footerScripts')
+    @parent
+@endsection

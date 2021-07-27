37e7aefa27bac520b3a9c1695c782e0a100cf5d5
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-07-28 00:39:29 +0100

    Used paginate in query.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::get();
+        $users = User::paginate(5);
         $user = User::first();
 
         return view('users.index', [

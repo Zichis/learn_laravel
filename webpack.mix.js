ac65fe8e94448cb7975184996929ad5f73e3ab06
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-07-28 00:36:44 +0100

    Updated mix.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ])
-    .vue();
+mix.js('resources/js/app.js', 'public/js').vue()
+    .sass('resources/scss/app.scss', 'public/css')
+    .options({
+        postCss: [
+            require('postcss-import'),
+            require('tailwindcss'),
+            require('autoprefixer')
+        ]
+    })
+    .sourceMaps()
+    .extract(['vue']);

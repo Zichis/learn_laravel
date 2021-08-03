d91487e80e252b11664788619fa75d73c2e0ae10
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-08-03 12:16:32 +0100

    Removed vendor and manifest.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,8 +12,6 @@
         @yield('section')
     </div>
     @section('footerScripts')
-        <script src="{{ mix('js/manifest.js') }}"></script>
-        <script src="{{ mix('js/vendor.js') }}"></script>
         <script src="{{ mix('js/app.js') }}"></script>
     @show
 </body>

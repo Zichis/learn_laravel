e3ad795338a23aa50ffc96b732377959b44e9b37
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-08-14 01:08:08 +0100

    Created contact test.

---
+++ tests/Feature/ContactTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ContactTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_contacts_page_can_be_rendered()
+    {
+        $response = $this->get('/contacts');
+
+        $response->assertStatus(200);
+    }
+}
